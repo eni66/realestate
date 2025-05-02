045d56834bc5cd1903acff687382d1dc9da40083
eni66 <enicancook@gmail.com>
2025-05-02 17:58:08 +0100

    product detail update

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -267,7 +267,7 @@ const ProductDetailsInfo = ({
             }
             onClick={() => setActive(1)}
           >
-            Product Details
+            Residence Details
           </h5>
           {active === 1 ? (
             <div className={`${styles.active_indicator}`} />
@@ -280,7 +280,7 @@ const ProductDetailsInfo = ({
             }
             onClick={() => setActive(2)}
           >
-            Product Reviews
+            Residence Reviews
           </h5>
           {active === 2 ? (
             <div className={`${styles.active_indicator}`} />
@@ -293,7 +293,7 @@ const ProductDetailsInfo = ({
             }
             onClick={() => setActive(3)}
           >
-            Seller Information
+            Agent Information
           </h5>
           {active === 3 ? (
             <div className={`${styles.active_indicator}`} />
@@ -330,7 +330,7 @@ const ProductDetailsInfo = ({
 
           <div className="w-full flex justify-center">
             {data && data.reviews.length === 0 && (
-              <h5>No Reviews have for this product!</h5>
+              <h5>No Reviews have for this residence!</h5>
             )}
           </div>
         </div>
@@ -358,14 +358,14 @@ const ProductDetailsInfo = ({
           </div>
           <div className="w-full 800px:w-[50%] mt-5 800px:mt-0 800px:flex flex-col items-end">
             <div className="text-left">
-              <h5 className="font-[600]">
+              {/* <h5 className="font-[600]">
                 Joined on:{" "}
                 <span className="font-[500]">
                   {data.shop?.createdAt?.slice(0, 10)}
                 </span>
-              </h5>
+              </h5> */}
               <h5 className="font-[600] pt-3">
-                Total Products:{" "}
+                Total residence:{" "}
                 <span className="font-[500]">
                   {products && products.length}
                 </span>
@@ -378,7 +378,7 @@ const ProductDetailsInfo = ({
                 <div
                   className={`${styles.button} !rounded-[4px] !h-[39.5px] mt-3`}
                 >
-                  <h4 className="text-white">Visit Shop</h4>
+                  <h4 className="text-white">Agent Profile</h4>
                 </div>
               </Link>
             </div>
