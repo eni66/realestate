3c74b1df927e12288a181bcb5f751f6f85814b2a
eni66 <enicancook@gmail.com>
2025-05-02 17:44:50 +0100

    product detail update

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -152,15 +152,15 @@ const ProductDetails = ({ data }) => {
                 <p>{data.description}</p>
                 <div className="flex pt-3">
                   <h4 className={`${styles.productDiscountPrice}`}>
-                    {data.discountPrice}$
+                    ${data.discountPrice}/month
                   </h4>
                   <h3 className={`${styles.price}`}>
-                    {data.originalPrice ? data.originalPrice + "$" : null}
+                    ${data.originalPrice ? data.originalPrice + " " : null}
                   </h3>
                 </div>
 
                 <div className="flex items-center mt-12 justify-between pr-3">
-                  <div>
+                  {/* <div>
                     <button
                       className="bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out"
                       onClick={decrementCount}
@@ -176,7 +176,7 @@ const ProductDetails = ({ data }) => {
                     >
                       +
                     </button>
-                  </div>
+                  </div> */}
                   <div>
                     {click ? (
                       <AiFillHeart
@@ -197,14 +197,14 @@ const ProductDetails = ({ data }) => {
                     )}
                   </div>
                 </div>
-                <div
+                {/* <div
                   className={`${styles.button} !mt-6 !rounded !h-11 flex items-center`}
                   onClick={() => addToCartHandler(data._id)}
                 >
                   <span className="text-white flex items-center">
                     Add to cart <AiOutlineShoppingCart className="ml-1" />
                   </span>
-                </div>
+                </div> */}
                 <div className="flex items-center pt-8">
                   <Link to={`/shop/preview/${data?.shop._id}`}>
                     <img
