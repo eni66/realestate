7c64c1ac2881198071ea9cf7692e9a74cf3b4b29
eni66 <enicancook@gmail.com>
2025-05-02 17:11:46 +0100

    product card update

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -89,7 +89,7 @@ const ProductCard = ({ data,isEvent }) => {
                 ${data.originalPrice === 0
                   ? data.originalPrice
                   : data.discountPrice}
-                /month
+              
               </h5>
               <h4 className={`${styles.price}`}>
                 ${data.originalPrice ? data.originalPrice + " /month" : null}
