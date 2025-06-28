bc48b3a9d43d3986b57b4768091b477f1e122b80
eni66 <enicancook@gmail.com>
2025-06-28 10:29:12 +0100

    Roomvella link update

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://realestate-vh8z.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://roomvella.com/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
