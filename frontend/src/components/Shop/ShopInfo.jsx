36d51a5db945874a4ebaf2e212d30b74a20fe39b
eni66 <enicancook@gmail.com>
2025-06-29 12:14:54 +0100

    Shop info update

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -64,6 +64,11 @@ const ShopInfo = ({ isOwner }) => {
         </p>
       </div>
       <div className="p-3">
+        <h5 className="font-[600]">Total Properties</h5>
+        <h4 className="text-[#000000a6]">{products && products.length}</h4>
+      </div>
+     {/* 
+      <div className="p-3">
         <h5 className="font-[600]">Address</h5>
         <h4 className="text-[#000000a6]">{data.address}</h4>
       </div>
@@ -71,15 +76,12 @@ const ShopInfo = ({ isOwner }) => {
         <h5 className="font-[600]">Phone Number</h5>
         <h4 className="text-[#000000a6]">{data.phoneNumber}</h4>
       </div>
-      <div className="p-3">
-        <h5 className="font-[600]">Total Properties</h5>
-        <h4 className="text-[#000000a6]">{products && products.length}</h4>
-      </div>
-      <div className="p-3">
+     
+     <div className="p-3">
         <h5 className="font-[600]">Ratings</h5>
         <h4 className="text-[#000000b0]">{averageRating}/5</h4>
       </div>
-      {/* <div className="p-3">
+      <div className="p-3">
         <h5 className="font-[600]">Joined On</h5>
         <h4 className="text-[#000000b0]">{data?.createdAt?.slice(0, 10)}</h4>
       </div> */}
