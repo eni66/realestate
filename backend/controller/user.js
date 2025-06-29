ff20c04e2d71c9eaf287ba3498edc43ff0f9ec02
eni66 <enicancook@gmail.com>
2025-06-29 19:54:21 +0100

    sameSite update

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -160,7 +160,7 @@ router.get(
       res.cookie("token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "lax",
         secure: true,
       });
       res.status(201).json({
