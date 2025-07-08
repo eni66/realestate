f08f66b02672849942cfafe14501f6bac52f28ce
eni66 <enicancook@gmail.com>
2025-07-08 14:38:40 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -28,9 +28,7 @@ const Agents = () => {
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
         {
             allAgents && allAgents.length !== 0 &&(
-              <>
-               {allAgents && allAgents.map((i, index) => <ShopInfo data={i} key={index} />)}
-              </>
+              <h1>Featured Products</h1>
             )
            }
         </div>
