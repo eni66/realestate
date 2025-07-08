e7a91d2ce60905f89029f07013c6641ba964469b
eni66 <enicancook@gmail.com>
2025-07-08 15:21:54 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -33,12 +33,8 @@ const Agents = () => {
         <div className={`${styles.heading}`}>
           {/* <h1>Featured Products</h1> */}
         </div>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-        {
-            sellers && sellers.length !== 0 &&(
-              <h1>Featured Products</h1>
-            )
-           }
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0"
+          <h1>Featured Products</h1>
         </div>
       </div>
     </div>
