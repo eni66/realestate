867233de75838cd042d133cfb8adff863a04fc8f
eni66 <enicancook@gmail.com>
2025-07-08 12:46:45 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -1,1 +1,34 @@
+import React, { useState } from "react";
+import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
+import styles from "../styles/styles";
 
+const AgentsPage = () => {
+  return (
+    <div>
+      <Header activeHeading={4} />
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
+
+const Faq = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const toggleTab = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab(0);
+    } else {
+      setActiveTab(tab);
+    }
+  };
+
+  return (
+    <div className={`${styles.section} my-8`}>
+      <h2 className="text-3xl font-bold text-gray-900 mb-8">Let's Talk!</h2>
+    </div>
+  );
+};
+
+export default AgentsPage;
