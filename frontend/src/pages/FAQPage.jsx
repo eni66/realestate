61613ecfad2521b7412eb8a70a787ff2b57a369e
eni66 <enicancook@gmail.com>
2025-07-08 10:42:57 +0100

    Update shop preview

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
+import ShopInfo from "../../components/Shop/ShopInfo";
 import styles from "../styles/styles";
 
 const FAQPage = () => {
@@ -26,6 +27,12 @@ const Faq = () => {
 
   return (
     <div className={`${styles.section} my-8`}>
+
+       <div className="w-full 800px:flex py-10 justify-between">
+          <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
+            <ShopInfo isOwner={false} />
+          </div>
+       </div>
       <h2 className="text-3xl font-bold text-gray-900 mb-8">Let's Talk!</h2>
       <div className="mx-auto space-y-4">
         {/* single Faq */}
