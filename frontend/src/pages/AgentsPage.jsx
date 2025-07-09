edc46f26d4a742ed77bd575bfb037f2be21d0eb2
eni66 <enicancook@gmail.com>
2025-07-09 09:13:09 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
-import { getAllAgents } from "../redux/actions/sellers";
-import ShopInfo from "../components/Shop/ShopInfo";
 import styles from "../styles/styles";
 
 
