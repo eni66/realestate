f09f9b1d11ceadb8f009e9d5dddd4795bbdb96cb
eni66 <enicancook@gmail.com>
2025-07-09 11:59:05 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import { getAllAgents } from "../redux/actions/sellers";
