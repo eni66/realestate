75a1181348e5d2f3b7c2e2117de943f441eab0ba
eni66 <enicancook@gmail.com>
2025-07-09 09:23:12 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -8,36 +8,11 @@ const AgentsPage = () => {
   return (
     <div>
       <Header activeHeading={4} />
-      <Agents />
+      
       <Footer />
     </div>
   );
 };
 
-const Agents = () => {
-
-  const [activeTab, setActiveTab] = useState(0);
-
-  const toggleTab = (tab) => {
-    if (activeTab === tab) {
-      setActiveTab(0);
-    } else {
-      setActiveTab(tab);
-    }
-  };
-   
-  return (
-    <div>
-      <div className={`${styles.section}`}>
-        <div className={`${styles.heading}`}>
-          {/* <h1>Featured Products</h1> */}
-        </div>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0"
-          <h1>Featured Products</h1>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default AgentsPage;
