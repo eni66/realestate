5c8f1d59d7053556981e0326372615b485b435cb
eni66 <enicancook@gmail.com>
2025-07-09 14:59:31 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { DataGrid } from "@material-ui/data-grid";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { Button } from "@material-ui/core";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import { getAllAgents } from "../redux/actions/sellers";
@@ -21,23 +24,71 @@ const AgentsPage = () => {
 const Agents = () => {
   const dispatch = useDispatch();
   const { sellers } = useSelector((state) => state.seller);
+  const [open, setOpen] = useState(false);
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
     dispatch(getAllAgents());
   }, [dispatch]);
 
+  const columns = [
+    { field: "id", headerName: "Seller ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "name",
+      headerName: "name",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      type: "text",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "address",
+      headerName: "Seller Address",
+      type: "text",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "joinedAt",
+      headerName: "joinedAt",
+      type: "text",
+      minWidth: 130,
+      flex: 0.8,
+    },
+    
+    ];
+
+  const row = [];
+  sellers &&
+  sellers.forEach((item) => {
+      row.push({
+        id: item._id,
+        name: item?.name,
+        email: item?.email,
+        joinedAt: item.createdAt.slice(0, 10),
+        address: item.address,
+      });
+    });
+
   return (
     <div className={`${styles.section} my-8`}>
       <h2 className="text-3xl font-bold text-gray-900 mb-8">Our Agents</h2>
 
-       <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-        {
-            sellers && sellers.length !== 0 &&(
-              <>
-               {sellers && sellers.map((i, index) => <ShopInfo data={i} key={index} />)}
-              </>
-            )
-           }
+        <div className="w-full min-h-[45vh] bg-white rounded">
+          <DataGrid
+            rows={row}
+            columns={columns}
+            pageSize={10}
+            disableSelectionOnClick
+            autoHeight
+          />
         </div>
       
     </div>
