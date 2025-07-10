2d7d63d07366a9ac1fc8ad8ac2823e7a2ba84a3c
eni66 <enicancook@gmail.com>
2025-07-10 21:46:13 +0100

    Update FAQPage

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -6,7 +6,7 @@ import styles from "../styles/styles";
 const FAQPage = () => {
   return (
     <div>
-      <Header activeHeading={4} />
+      <Header activeHeading={5} />
       <Faq />
       <Footer />
     </div>
