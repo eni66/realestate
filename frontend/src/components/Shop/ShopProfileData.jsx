8bc684f236512fe094debbc18493baf583ce69b1
eni66 <enicancook@gmail.com>
2025-07-14 11:56:49 +0100

    Update ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { getAllProductsShop } from "../../redux/actions/product";
 import styles from "../../styles/styles";
 import ProductCard from "../Route/ProductCard/ProductCard";
+import EventCard from "../Events/EventCard";
 import Ratings from "../Products/Ratings";
 import { getAllEventsShop } from "../../redux/actions/event";
 
@@ -84,7 +85,7 @@ const ShopProfileData = ({ isOwner }) => {
           <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
             {events &&
               events.map((i, index) => (
-                <ProductCard
+                <EventCard
                   data={i}
                   key={index}
                   isShop={true}
