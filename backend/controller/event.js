001d307d3c7cb35366776a708031cae4c90b3ea7
eni66 <enicancook@gmail.com>
2025-07-16 09:55:57 +0100

    Update event

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -44,7 +44,7 @@ router.post(
 
         const event = await Event.create(productData);
 
-        res.status(201).json({
+        res.status(200).json({
           success: true,
           event,
         });
