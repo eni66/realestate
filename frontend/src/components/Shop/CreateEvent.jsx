e4044f42688ffca552497551b6e87ad1eebc7aad
eni66 <enicancook@gmail.com>
2025-07-16 08:59:21 +0100

    Update CreateEvent

--- frontend/src/components/Shop/CreateEvent.jsx
+++ frontend/src/components/Shop/CreateEvent.jsx
@@ -99,9 +99,6 @@ useEffect(() => {
       Finish_Date: endDate?.toISOString(),
     };
     dispatch(createevent(data));
-    toast.success("Promotion created successfully!");
-      navigate("/dashboard-events");
-      window.location.reload();
   };
 
   return (
