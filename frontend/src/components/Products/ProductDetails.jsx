6c5feb810fe010f45f609322b5d4374e5766a5aa
eni66 <enicancook@gmail.com>
2025-07-17 15:35:19 +0100

    Update ProductDetails

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -122,7 +122,7 @@ const ProductDetails = ({ data }) => {
                 <img
                   src={`${data && data.images[select]?.url}`}
                   alt=""
-                  className="w-[80%] object-cover"
+                  className="w-[100%] object-cover"
                 />
                 <div className="w-full flex">
                   {data &&
