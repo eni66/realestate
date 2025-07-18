14eee7a489eedbdf769c93d76813869d71bc2572
eni66 <enicancook@gmail.com>
2025-07-18 14:48:11 +0100

    Update ProductCard

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -97,15 +97,16 @@ const ProductCard = ({ data,isEvent }) => {
 
           <div className="py-2 flex items-center justify-between">
             <div className="flex">
+              <h4 className={`${styles.price}`}>
+                €{data.originalPrice ? data.originalPrice + " " : null}
+              </h4>
               <h5 className={`${styles.productDiscountPrice}`}>
                 €{data.originalPrice === 0
                   ? data.originalPrice
                   : data.discountPrice}
                 /month
               </h5>
-              <h4 className={`${styles.price}`}>
-                €{data.originalPrice ? data.originalPrice + " " : null}
-              </h4>
+              
             </div>
             {/* <span className="font-[400] text-[17px] text-[#68d284]">
               {data?.sold_out} sold
