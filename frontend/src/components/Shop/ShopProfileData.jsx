37ba3dc2b522255eb41621215659133a7595b87d
eni66 <enicancook@gmail.com>
2025-07-18 17:03:34 +0100

    Update ShopProfileData

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -47,7 +47,7 @@ const ShopProfileData = ({ isOwner }) => {
             </h5>
           </div>
 
-          <div className="flex items-center" onClick={() => setActive(3)}>
+          {/*<div className="flex items-center" onClick={() => setActive(3)}>
             <h5
               className={`font-[600] text-[20px] ${
                 active === 3 ? "text-red-500" : "text-[#333]"
@@ -55,7 +55,7 @@ const ShopProfileData = ({ isOwner }) => {
             >
               Reviews
             </h5>
-          </div>
+          </div>*/}
         </div>
         <div>
           {isOwner && (
