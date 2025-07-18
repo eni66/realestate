60f009395f6c05318d1c4a5c89cb75ed03679186
eni66 <enicancook@gmail.com>
2025-07-18 17:00:41 +0100

    Update ProductCard

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -72,9 +72,11 @@ const ProductCard = ({ data,isEvent }) => {
             className="w-full h-[170px] object-cover"
           />
         </Link>
+        {/*
         <Link to={`/shop/preview/${data?.shop._id}`}>
           <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
         </Link>
+        */}
         <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
           <h4 className="pb-3 font-[500]">
             {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
