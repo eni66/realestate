247f1f7d7a169e9f4ee6ce77cfe9e1cc750ed445
eni66 <enicancook@gmail.com>
2025-07-18 14:32:22 +0100

    Update CountDown

--- frontend/src/components/Events/CountDown.jsx
+++ frontend/src/components/Events/CountDown.jsx
@@ -43,7 +43,7 @@ const CountDown = ({ data }) => {
     }
 
     return (
-      <span className="text-[10px] text-[#f6b100]">
+      <span className="text-[3px] text-[#f6b100]">
         {timeLeft[interval]} {interval}{" "}
       </span>
     );
