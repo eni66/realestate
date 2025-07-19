6909aba549c83fd058f029fac20fa1dc4cc1c399
eni66 <enicancook@gmail.com>
2025-07-19 11:07:47 +0100

    Update AgentsPage

--- frontend/src/pages/AgentsPage.jsx
+++ frontend/src/pages/AgentsPage.jsx
@@ -33,7 +33,7 @@ const Agents = () => {
 
   return (
     <div className={`${styles.section} my-8`}>
-      <h2 className="text-3xl font-bold text-gray-900 mb-8">Our Agents</h2>
+      {/* <h2 className="text-3xl font-bold text-gray-900 mb-8">Agents</h2> */}
       
       <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
         {
