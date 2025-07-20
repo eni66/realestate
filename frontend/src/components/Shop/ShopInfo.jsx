a3d7ddcbd66b4efbea71a9e74213aadadea9548b
eni66 <enicancook@gmail.com>
2025-07-20 09:06:22 +0100

    Update ShopInfo

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -60,7 +60,7 @@ const ShopInfo = ({ isOwner }) => {
           />
         </div>
         <h3 className="text-center py-2 text-[20px]">{data.name}</h3>
-        <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+        <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center text-center">
           {data.description}
         </p>
         <div className="flex item-center justify-center">
